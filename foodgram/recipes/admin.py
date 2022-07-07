45717b443d769dbd3365079fada4ff0e1d3bdc5d
Владимир Шалдыкин <shaldykin@gmail.com>
2022-07-07 16:47:54 +0300

    after first migration

--- foodgram/recipes/admin.py
+++ foodgram/recipes/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+     Favorite, Ingredient, IngredientAmount, Recipe, ShoppingList, Tag
+)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'name',
+        'pub_date',
+        'image',
+        'description',
+        'cooking_time',
+        'pk'
+    )
+    search_fields = ('author', 'name')
+    list_filter = ('author', 'name', 'tags')
+    empty_value_display = '-пусто-'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('title', 'measurement_unit', 'pk')
+    search_fields = ('title',)
+    list_filter = ('title',)
+    empty_value_display = '-пусто-'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug', 'pk')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class IngredientAmountAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount', 'pk')
+    search_fields = ('recipe',)
+    list_filter = ('recipe',)
+    empty_value_display = '-пусто-'
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'recipe',
+    )
+    search_fields = ('recipe',)
+    list_filter = ('recipe',)
+    empty_value_display = '-пусто-'
+
+
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'user',
+        'recipe',
+    )
+    search_fields = ('recipe',)
+    list_filter = ('recipe',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(IngredientAmount, IngredientAmountAdmin)
+admin.site.register(ShoppingList, ShoppingListAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(Tag, TagAdmin)
