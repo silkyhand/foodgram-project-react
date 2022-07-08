13fd5db6c98441eb076dd3c15981c42df5b1fce3
Владимир Шалдыкин <shaldykin@gmail.com>
2022-07-08 17:11:29 +0300

    nothing

--- foodgram/users/views.py
+++ foodgram/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import filters, permissions, status, viewsets
+from rest_framework.decorators import action, api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+from . models import CustomUser
+from . serializers import TokenSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def get_jwt_token(request):
+    serializer = TokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data.get('username')
+    email = serializer.validated_data.get('email')
+    user = get_object_or_404(CustomUser, username=username, email=email)
+    if user:
+        token = AccessToken.for_user(user)
+        return Response({'token': f'{token}'}, status=status.HTTP_201_CREATED)
+    
