2b556876483165a2fc53b3f1254197a5790f0ed5
Владимир Шалдыкин <shaldykin@gmail.com>
2022-07-20 17:10:04 +0300

    for third review

--- foodgram/users/views.py
+++ foodgram/users/views.py
@@ -4,11 +4,10 @@ from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from users.models import CustomUser
 
 from api.pagination import ProjectPagination
 from api.serializers import FollowSerializer
-from users.models import Follow
+from users.models import CustomUser, Follow
 
 
 class CustomUserViewSet(UserViewSet):
@@ -29,14 +28,14 @@ class CustomUserViewSet(UserViewSet):
                 return Response({
                     'errors': 'Вы уже подписаны на данного пользователя'
                 }, status=status.HTTP_400_BAD_REQUEST)
-                       
+
             follow = Follow.objects.create(user=user, author=author)
             serializer = FollowSerializer(
                 follow, context={'request': request}
             )
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        elif request.method == "DELETE":
+        if request.method == "DELETE":
             if user == author:
                 return Response({
                     'errors': 'Вы не можете отписываться от самого себя'
