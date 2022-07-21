84197b0c6a66deec29eee7d2287cb9ef1d6cf680
Владимир Шалдыкин <shaldykin@gmail.com>
2022-07-21 23:31:59 +0300

    fixed bugs with localhost

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE',
-                            default='django.db.backends.postgresql_psycopg2'),
-        'NAME': os.getenv('DB_NAME', default='foodgram'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='lomonosova372'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432')
+        'ENGINE': os.getenv('DB_ENGINE'),
+                            # default='django.db.backends.postgresql_psycopg2'),
+        'NAME': os.getenv('DB_NAME'), # default='foodgram'),
+        'USER': os.getenv('POSTGRES_USER'), # default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'), # default='lomonosova372'),
+        'HOST': os.getenv('DB_HOST'),# default='db'),
+        'PORT': os.getenv('DB_PORT') # default='5432')
     }
 }
 
