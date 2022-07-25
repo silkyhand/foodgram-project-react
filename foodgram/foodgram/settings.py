be472349cb9d89699506c0f95fc40a673e73c0c0
Владимир Шалдыкин <shaldykin@gmail.com>
2022-07-25 16:32:45 +0300

    workflows

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'vabz0bm(@jzxw!pruv)5z*@om7jjqjwew-5kxy@f2ai9j$uy4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
